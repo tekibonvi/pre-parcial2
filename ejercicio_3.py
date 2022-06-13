83c83adbb9408a96499981c3ea8ef3c062cd8045
Ezequias Bonvissuto <ezequiasbonvissuto@gmail.com>
2022-06-13 19:26:59 -0300

    Ejercicio_3 completo

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -1,6 +1,6 @@
 class Persona ():
 
-    def __init__(self, nombre,edad,dni):
+    def __init__(self, nombre = "",edad ="",dni= ""):
         self.nombre = nombre
         self.edad = edad
         self.dni = dni
@@ -8,8 +8,8 @@ class Persona ():
 
     def mostrar(self):
         print (f"el nombre es : {self.nombre}")
-        print (f"el nombre es : {self.edad}")
-        print (f"el nombre es : {self.dni}")
+        print (f"La edad es: : {self.edad}")
+        print (f"El dni es : {self.dni}")
 
     def esMayorEdad(self):
         if self.edad >= 18:
@@ -19,5 +19,34 @@ class Persona ():
         
 class Cuenta():
     
-    def __init__(self):
-        pass
+    def __init__(self, titular,cantidad = 0):
+        self.titular = titular
+        self.cantidad = cantidad
+
+    def mostrar(self):
+        
+        print(f"El titular es: {self.titular}")
+        print(f"El saldo es: {self.cantidad}")
+
+    def ingresar(self,cantidad):
+        self.cantidad = cantidad + self.cantidad
+
+    def retirar(self,cantidad):
+        self.cantidad = self.cantidad - cantidad
+
+
+persona1 = Persona
+
+
+
+
+cuenta_uno = Cuenta("Ezequias Bonvissuto")
+
+cuenta_uno.mostrar()
+cuenta_uno.ingresar(500)
+cuenta_uno.mostrar()
+
+cuenta_uno.retirar(300)
+cuenta_uno.mostrar()
+
+
